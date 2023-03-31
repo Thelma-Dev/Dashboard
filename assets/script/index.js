0a45d14151e2ff7c6a6ded12886c91b1f946afd5
Thelma-Dev <osifoaisosa@gmail.com>
2023-03-31 13:29:03 -0500

    Update ScreenDisplay

--- assets/script/index.js
+++ assets/script/index.js
@@ -13,6 +13,14 @@ function select(selector, parent = document) {
     return parent.querySelector(selector);
 }
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+      
+window.addEventListener('resize', () => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 
 // Selection
 const valueGraph = getElement('graph');
